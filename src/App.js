d34e2aa2b53c830d610732fc198f46908f3c0742
Tatooine93 <nowakantoine93@gmail.com>
2022-07-06 14:05:38 +0200

    first push

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React ,{useState, useRef, useEffect} from 'react';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+function App() {
+
+  const [todos, setTodos] = useState([
+ /*  {id: 1, name:'Todo 1', complete: false},
+  {id: 2, name:'Todo 2', complete: false} */
+  ]);
+  
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+  
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+  
+
+  function addTodo() {
+    const name = todoNameRef.current.value
+
+    if (name === '') {return console.log(`You must enter a task`)}
+    
+    setTodos (prevTodos => {
+        return ([...prevTodos, {id : uuidv4(), name: name, complete: false}])
+      })
+
+    todoNameRef.current.value = null
+  }
+  
+  return (
+    <>
+      <input ref={todoNameRef} type="text"/>
+      <button onClick={addTodo}>Add a Todo</button>
+      <button>Clear Completed Task</button>
+      <TodoList todos={todos}/>
+      <div>{todos.length} left to do</div>
+    </>
+  );
+}
+
+export default App;
